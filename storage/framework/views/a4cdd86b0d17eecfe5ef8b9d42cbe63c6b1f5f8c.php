62332a9b3aa2150b0c21df5ce2739412550b8635
yuting huang <d1114182001@gm.lhu.edu.tw>
2024-10-25 14:09:25 +0800

    add ignore file (test to upload)

---
+++ storage/framework/views/a4cdd86b0d17eecfe5ef8b9d42cbe63c6b1f5f8c.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="zh-Hant">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>可持續發展目標 (SDGs)-優質教育</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            line-height: 1.6;
+        }
+        h1, h2 {
+            color: #2E8B57;
+        }
+        .goal {
+            margin: 10px 0;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }.test{
+            overflow: visible;
+        }
+    </style>
+</head>
+<body>
+
+    <h1>可持續發展目標 (SDGs)-優質教育</h1>
+    <img src="pictures/62f0da478154a8e89b69f0a4_SDG目標04.png" alt="Cover Image"/>
+    <p>可持續發展目標（Sustainable Development Goals, SDGs）是聯合國於2015年制定的17項全球性目標，旨在解決貧困、不平等和氣候變化等問題，促進可持續發展，直到2030年。</p>
+
+    
+
+</body>
+</html>
+<?php /**PATH C:\wamp64\www\team11\resources\views/intro_sdgs.blade.php ENDPATH**/ ?>
