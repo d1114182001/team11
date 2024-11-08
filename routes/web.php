28c20e7be958333864dac3d083e4c17386a0eadc
D1114182004 <D1114182004@gm.lhu.edu.tw>
2024-11-08 16:03:19 +0800

    mido

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EducationContro;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,5 @@ Route::get('/', function () {
 });
 
 
-Route::get('/sdgs', function () {
-    return view('intro_sdgs');
-});
+
+Route::get('education',[EducationContro::class,"index"]);
