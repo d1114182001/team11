8e5f7f864fd57973599e4546fb8b99af53c94387
yuting huang <d1114182001@gm.lhu.edu.tw>
2024-11-12 12:02:15 +0800

    Add navbar

---
+++ resources/views/particials/navbar.blade.php
@@ -0,0 +1,17 @@
+<!-- navbar.blade.php -->
+<nav class="navbar">
+    <div class="container">
+        <img src="/pictures/E_SDG_Icons-04.jpg" width="80" alt="dd"/>
+        <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">服務</a>
+            </li>
+            <li>
+              <a href="#">關於</a>
+            </li>
+          </ul>    
+    </div>
+</nav>
--- resources/views/populationEducations/populationEducations.blade.php
+++ resources/views/populationEducations/populationEducations.blade.php
@@ -1,25 +1,33 @@
 <html>
     <head>
         <title>我國教育人口</title>
+        <link rel="stylesheet" href="{{ asset('/app.css') }}">
     </head>
+    
     <body>
+        @include('particials.navbar')
         <h1>我國教育人口</h1>
-        <table border ="1">
-            @foreach ($pop as $populations)
-                <tr>
-                    <td>{{ $populations->region }}</td>
-                    <td>{{ $populations->total }}</td>
-                    <td>{{ $populations->edu_jr_high_below_total }}</td>
-                    <td>{{ $populations->edu_college_above_total }}</td>
-                    <td>{{ $populations->age_15_24_total }}</td>
-                    <td>{{ $populations->age_25_44_total }}</td>
-                    <td>{{ $populations->age_45_64_total }}</td>
-                    <td>{{ $populations->age_65_above }}</td>
-                    
-                </tr>
-            @endforeach
-        </table>
         
         
+        <div class="content">
+            <div class="a">
+                <img src="/pictures/62f0da478154a8e89b69f0a4_SDG目標04.png" alt="Cover Image"/>
+            </div>
+            <table border ="1">
+                @foreach ($pop as $populations)
+                    <tr>
+                        <td>{{ $populations->region }}</td>
+                        <td>{{ $populations->total }}</td>
+                        <td>{{ $populations->edu_jr_high_below_total }}</td>
+                        <td>{{ $populations->edu_college_above_total }}</td>
+                        <td>{{ $populations->age_15_24_total }}</td>
+                        <td>{{ $populations->age_25_44_total }}</td>
+                        <td>{{ $populations->age_45_64_total }}</td>
+                        <td>{{ $populations->age_65_above }}</td>
+                        
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </body>
 </html>
