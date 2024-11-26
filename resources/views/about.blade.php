e5b2202a274bacea380362612f0b7adbaa40b200
yuting huang <d1114182001@gm.lhu.edu.tw>
2024-11-26 12:02:34 +0800

    更換about header picture,下次調整

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,15 +3,15 @@
 @section('title', 'SDG 介紹首頁')
 
 @section('header')
-<header class="sdg-header">
-    <div class="sdg-header-content">
-        <img src="/pictures/sdg_nav.png" alt="SDG Logo" class="sdg-logo">
-        <div class="sdg-text">
-            <h1>可持續發展目標 (SDG)</h1>
-            <p>我們共同的未來，為了社會、環境與經濟的可持續發展。</p>
-        </div>
+
+<div class="sdg-header-content">
+    <img src="/pictures/sdg_2.png" alt="SDG Logo" class="sdg-logo">
+    <div class="sdg-text">
+        <h1>可持續發展目標 (SDG)</h1>
+        <p>我們共同的未來，為了社會、環境與經濟的可持續發展。</p>
     </div>
-</header>
+</div>
+
 
 @endsection
 @section('navbar')
@@ -19,5 +19,5 @@
 @endsection
 @section('content')
     <h1>歡迎來到 SDG 介紹首頁</h1>
-    <p>這裡可以放一些簡單的靜態內容。</p>
+    <p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p><p>這裡可以放一些簡單的靜態內容。</p>
 @endsection
